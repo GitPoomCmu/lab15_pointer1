a1c8b07db8adab1a491d53d43ee70cccea0fcdc6
GitPoomCmu <152594220+GitPoomCmu@users.noreply.github.com>
2024-01-26 16:30:28 +0700

    Update lab15_1.cpp

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,9 +1,27 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int n = 5;
 
+int main(){
+    int a = 5;
+    char b = 'A';
+    char &c = b;
+    
+    int *x = &a;
+    char *y = &b;
+    int **z = &x;
 	
+	for(int i=0; i<n; i++){
+	    cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl;
+	    if(i==0){
+	        cout << &a << " " << &b << " " << &c << " " << &x << " " << &y << " " << &z << endl;
+	        c = 'F';
+	    }
+	    if(i==1) *y = 'W';
+	    if(i==2) *x = 6;
+	    if(i==3) **z = 7;
+	}
 	
 	return 0;
 }
